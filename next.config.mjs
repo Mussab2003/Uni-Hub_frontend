65ce1199f147ff901e40ebe42070495774f2beb3
Mussab2003 <muhammadmussab12345@gmail.com>
2024-09-08 18:12:13 +0500

    Github Pages Setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/uni-hub',
+    output: 'export',
+    reactStrictMode: true,
+};
 
 export default nextConfig;
