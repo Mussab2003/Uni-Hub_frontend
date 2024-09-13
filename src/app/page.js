a1e0afe3acc168250d5de015e8af67d96ca49fa8
Mussab2003 <muhammadmussab12345@gmail.com>
2024-09-13 18:21:51 +0500

    Responsive Div

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,12 @@ import { IoDocumentText } from "react-icons/io5";
 const Home = () => {
   return (
     <div className='mx-2'>
-      <div className='dark:bg-gradient-to-tr from-dodgerBlue to-darkBlack bg-[#d0f4d4] px-20 flex justify-evenly items-center w-full h-96 rounded-b-2xl'>
+      <div className='p-2 dark:bg-gradient-to-tr from-dodgerBlue to-darkBlack bg-[#d0f4d4] px-20 flex  flex-row  sm:justify-center md:justify-evenly lg:justify-evenly items-center w-full min-h-80 md:h-96 lg:h-96 rounded-b-2xl'>
         <div className='flex flex-col'>
-            <h1 className='font-bold text-5xl dark:text-dark-text'>View and upload <br/>repositories, create<br/>quizzes and notes!</h1>
+            <h1 className='font-bold text-4xl md:text-5xl lg:text-5xl dark:text-dark-text'>View and upload <br/>repositories, create<br/>quizzes and notes!</h1>
         </div>
-        <div>
-          <IoDocumentText size={250}/>
+        <div className='mt-2'>
+          <IoDocumentText size={250} className='hidden lg:flex md:flex'/>
         </div>
 
       </div> 
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
   ],
   theme: {
     extend: {
+      screens: {
+        'sm' : '640px',
+        'md' : '768px',
+        'lg' : '1024px',
+        'xl' : '1280px',
+      },
       colors: {
         //Light Theme Colors
         primary: "#d0f4d4",
