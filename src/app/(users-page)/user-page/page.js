8c2a5cb6a1285fd52ea5a119d5c08248a2028f5c
Mussab2003 <muhammadmussab12345@gmail.com>
2024-10-10 19:08:21 +0500

    Added New Repo Form

--- src/app/(users-page)/user-page/page.js
+++ src/app/(users-page)/user-page/page.js
@@ -1,31 +1,63 @@
 // app/user/page.js
 "use client";
-import React, { useEffect } from "react";
-import axios from "axios";
+import React, { useEffect, useState } from "react";
 import Hero from "@/components/pages/user_page/hero";
-import Repositories from "@/components/pages/user_page/repositories";
+import Repositories from "@/components/pages/user_page/repository_section";
 import { useAuth } from "@/context/auth_context";
+import LinearProgress from "@mui/material/LinearProgress";
+import RepositorySection from "@/components/pages/user_page/repository_section";
+import ChildDialog from "@/components/pages/user_page/create_repo_dialog";
 
 const UserPage = () => {
-  const { name, token, isGoogle, loading } = useAuth();
+  const { name, token, loading } = useAuth();
+  const [states, setStates] = useState({
+    isDialogOpen: false,
+    formType: "",
+  })
 
-  if (!loading) {
-    if (name == null || token == null) {
+  useEffect(() => {
+    if (!loading && (name == null || token == null)) {
       window.location.href = "/home";
     }
-  }
-  console.log(name, token)
+  }, [loading, name, token]);
 
-  return (
-    <div className="min-h-screen">
-      <section className="pt-24 flex" id="home">
-        <Hero />
-      </section>
-      <section className="pt-14 min-h-screen flex" id="home">
-        <Repositories />
-      </section>
-    </div>
-  );
+  const handleClickNewRepo = () => {
+    setStates((prev) => ({...prev, isDialogOpen: true, formType: "new"}))
+  }
+
+  // Show a progress bar while loading
+  if (loading) {
+    return (
+      <div className="min-h-screen flex justify-center items-center">
+        <LinearProgress />
+      </div>
+    );
+  }
+
+  if(token && name){
+
+    return (
+      <>
+        <div className="min-h-screen w-full">
+          
+            <Hero />
+    
+          <section className="pt-14  flex" id="home">
+            <RepositorySection handleClickNewRepo={handleClickNewRepo} />
+          </section>
+        </div>
+        <ChildDialog
+        isOpen={states.isDialogOpen}
+        onClose={() =>
+          setStates((prev) => ({ ...prev, isDialogOpen: false, formType: "" }))
+        }
+        formType={states.formType}
+        //switchForm={handleSwitchForm}
+      />
+      </>
+
+    );
+  }
 };
 
 export default UserPage;
--- src/components/pages/landing_page/features.js
+++ src/components/pages/landing_page/features.js
@@ -6,7 +6,7 @@ import React from "react";
 
 const Features = ({ title, description, color, children }) => {
   return (
-      <Card className={`w-full md:lg:w-1/4 h-72 p-4 flex flex-col justify-evenly gap-5 `} style={{backgroundColor: color}}>
+      <Card className={`w-full md:lg:w-1/4 min-h-auto md:h-80 lg:h-auto p-4 flex flex-col justify-evenly gap-5 `} style={{backgroundColor: color}}>
           <h1 className="text-2xl font-semibold">{title}</h1>
           <p className="font-medium">{description}</p>
           <hr className="border-1 border-black"/>
