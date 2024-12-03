aa0d1399d0ceeda2ca4cc02718028c94eb61c0f5
Mussab2003 <muhammadmussab12345@gmail.com>
2024-12-04 02:27:01 +0500

    Final V1

--- src/components/pages/repo_page/comment_section.jsx
+++ src/components/pages/repo_page/comment_section.jsx
@@ -99,7 +99,7 @@ const CommentSection = ({ repo_id, token }) => {
           {commentData.length == 0 && (
             <div className="flex justify-center pb-8">
 
-              <p className="text-xl font-medium ">No Comments</p>
+              <p className="text-xl font-medium dark:text-white">No Comments</p>
             </div>
           )}
         </div>
